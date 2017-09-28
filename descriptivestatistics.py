e3116807624d5c2eede6171af9ee7e62cec81fb6
wren saylor <sayloren@gmail.com>
2017-09-27 23:41:10 -0400

    uniform naming of files, added two files to make scripts in

---
+++ descriptivestatistics.py
@@ -0,0 +1,6 @@
+"""
+Script to run some descriptive statistics if the column is an int/float
+
+Wren Saylor
+September 27 2017
+"""
---
+++ linegraphfacetgrid.py
@@ -0,0 +1,6 @@
+"""
+Script to make a simple correlative line plot for each two int/float columns in file
+
+Wren Saylor
+September 27 2017
+"""
